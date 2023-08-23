0ecaa4921e07030bfa552f22693706285c141cb5
yoo-hyunseung <hsung122@naver.com>
2023-08-24 00:21:01 +0900

    공지,묻고답하기

--- Tiles/src/main/java/com/sist/web/landboardController.java
+++ Tiles/src/main/java/com/sist/web/landboardController.java
@@ -20,4 +20,17 @@ public class landboardController {
 		model.addAttribute("no", no);
 		return "landboard/landdetail";
 	}
+/*
+ * <li><a href="../sotong/haeyo.do">소통게시판</a></li>
+<li><a href="../landboard/notificationboard.do">공지게시판</a></li>
+<li><a href="../landboard/qna.do">묻고답하기</a></li>
+ */
+	@GetMapping("landboard/notificationboard.do")
+	public String landboard_notificationboard(){
+		return "landboard/notificationboard";
+	}
+	@GetMapping("landboard/qna.do")
+	public String landboard_qna() {
+		return "landboard/qna";
+	}
 }
