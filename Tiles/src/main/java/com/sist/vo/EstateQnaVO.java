b1cab824ec4ad915c386a3794dc4b0b6a4e7fa80
chominseok1 <mins2713@naver.com>
2023-08-24 09:41:27 +0900

    부동산 질문답변 VO

---
+++ Tiles/src/main/java/com/sist/vo/EstateQnaVO.java
@@ -0,0 +1,21 @@
+package com.sist.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/*
+ * NO NUMBER ,
+  id varchar2(100) CONSTRAINT eq_id_nn NOT NULL,
+  question CLOB CONSTRAINT eq_qs_nn NOT NULL ,
+  company varchar2(100) CONSTRAINT eq_cp_nn NOT NULL,
+  answer clob DEFAULT '답변대기',
+  CONSTRAINT eq_no_pk PRIMARY KEY(no),
+  CONSTRAINT eq_id_fk FOREIGN KEY(id) REFERENCES amem(id) ON DELETE CASCADE
+ */
+@Getter
+@Setter
+// answer 디폴트: 답변대기 관리자에서 => 글로 변경
+public class EstateQnaVO {
+ private int no;
+ private String id,question,company,answer;
+}
